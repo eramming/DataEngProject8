80a71ec4e0be0ff691d3794d1470cc8c5831e9c1
Jacob Snider <jnsnider90@gmail.com>
2026-04-09 11:29:03 -0400

    Fixes and error handling for the airflow integration.

--- backend/PostgresClient.py
+++ backend/PostgresClient.py
@@ -1,22 +1,37 @@
 import psycopg2
+import time
 from psycopg2.extras import RealDictCursor
 
 
 class PostgresClient:
-
     def __init__(self):
-        self.conn = psycopg2.connect(
-            dbname="jhu",
-            user="jhu",
-            password="jhu123",
-            host="postgres",
-            port="5432"
-        )
+        retries = 5
+        for i in range(retries):
+            try:
+                self.conn = psycopg2.connect(
+                    dbname="jhu",
+                    user="jhu",
+                    password="jhu123",
+                    host="postgres",
+                    port="5432"
+             )
+                print("Connected to Postgres!")
+                break
+            except psycopg2.OperationalError as e:
+                print(f"Retry {i+1}/{retries}...")
+                time.sleep(5)
+        else:
+            raise Exception("Could not connect to Postgres")
 
     def _fetch_all(self, query: str) -> list:
-        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute(query)
-            return cur.fetchall()
+        try:
+            with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute(query)
+                return cur.fetchall()
+        except Exception as e:
+            self.conn.rollback()
+            print(f"Query failed: {e}")
+            raise
 
     def get_venues(self) -> list:
         return self._fetch_all("SELECT * FROM mls_analysis.venues")
